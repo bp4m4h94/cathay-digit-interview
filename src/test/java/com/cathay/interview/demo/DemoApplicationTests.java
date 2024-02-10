21dbe42151c1824d38988b4e0dc12f0b693080aa
Ryan Tsai <bp4m4h94@gmail.com>
2024-02-10 22:35:01 +0800

    test: add test case

--- src/test/java/com/cathay/interview/demo/DemoApplicationTests.java
+++ src/test/java/com/cathay/interview/demo/DemoApplicationTests.java
@@ -7,6 +7,7 @@ import com.cathay.interview.remote.dto.BitcoinPriceDataDto;
 import com.cathay.interview.remote.dto.CurrencyInfoDto;
 import com.cathay.interview.remote.enums.CurrencyType;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -27,6 +28,31 @@ class DemoApplicationTests {
         BitcoinPriceDataDto deserialize = coinDeskService.deserialize();
         CurrencyInfoDto expected = deserialize.getBpi().get(CurrencyType.USD);
         assertEquals(expected.getCode(), currency != null ? currency.getCode().toString() : null);
+    }
+
+    @Test
+    @DisplayName("測試呼叫查詢幣別對應表資料 API，並顯示其內容")
+    void testFindNewApi() throws JsonProcessingException {
+    }
+    @Test
+    @DisplayName("測試呼叫新增幣別對應表資料 API。")
+    void testInsertNewApi() throws JsonProcessingException {
+    }
+    @Test
+    @DisplayName("測試呼叫更新幣別對應表資料 API，並顯示其內容。")
+    void testUpdateNewApi() throws JsonProcessingException {
+    }
+    @Test
+    @DisplayName("測試呼叫刪除幣別對應表資料 API。")
+    void testDeleteNewApi() throws JsonProcessingException {
+    }
+    @Test
+    @DisplayName("測試呼叫 coindesk API，並顯示其內容")
+    void testCoinDeskApi() throws JsonProcessingException {
+    }
+    @Test
+    @DisplayName("測試呼叫資料轉換的 API，並顯示其內容")
+    void testNewApi() throws JsonProcessingException {
     }
 
 }
